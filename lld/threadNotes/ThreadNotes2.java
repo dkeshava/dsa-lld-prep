ab71c470c6a76a411e0846b86e6df79bea536334
Dhondi Keshava <dhondikeshava12k@gmail.com>
2026-06-21 23:42:34 +0530

    Day 32: Threads wait and lock practice

--- lld/threadNotes/ThreadNotes2.java
+++ lld/threadNotes/ThreadNotes2.java
@@ -1,7 +1,9 @@
 class MyThread extends Thread{
     @Override
     public void run(){
-        System.out.println("run thread = "+Thread.currentThread().getName());
+        for(int i=1;i<=5;i++){
+            System.out.println(Thread.currentThread().getName()+" : "+i);
+        }
     }
 }
 public class ThreadNotes2 {
@@ -10,6 +12,10 @@ public class ThreadNotes2 {
         System.out.println("main thread = "+Thread.currentThread().getName());
         t.start();
         // t.start();
-        System.out.println("main thread = "+Thread.currentThread().getName());
+        for(int i=1;i<=5;i++){
+            System.out.println(
+                Thread.currentThread().getName()+" : "+i
+            );
+        }
     }
 }
---
+++ lld/threadNotes/ThreadNotes3.java
@@ -0,0 +1,72 @@
+class MyTask implements Runnable{
+    public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println("Runnable "+Thread.currentThread().getName()+" is running "+i);
+        }
+    }
+}
+
+class Counter{
+    int count=0;
+    public synchronized void increment(){
+        count++;
+    }
+}
+
+class SharedResource{
+    private boolean available=false;
+    public synchronized void waitForResource() throws InterruptedException{
+        while(!available){
+            System.out.println("Waiting...");
+            wait();
+            //Thread.sleep(1000);
+        }
+        Thread.sleep(200);
+        System.out.println("Resource available now!");
+    }   
+    public synchronized void makeAvailable(){
+        available=true;
+        System.out.println("Making resource available");
+        notify();
+    }
+}
+
+public class ThreadNotes3 {
+    public static void main(String[] args) throws InterruptedException {
+        //MyTask task=new MyTask();
+        // Counter counter=new Counter();
+        // Runnable task=()->{
+        //     for(int i=0;i<10000;i++){
+        //         counter.increment();
+        //     }
+        // };
+
+        // Thread thread1=new Thread(task);
+        // Thread thread2=new Thread(task);
+
+        // thread1.start();
+        // thread2.start();
+
+        // thread1.join();
+        // thread2.join();
+        // System.out.println(counter.count);
+        SharedResource resource=new SharedResource();
+        Thread consumer=new Thread(()->{
+            try {
+                resource.waitForResource();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread producer=new Thread(()->{
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            resource.makeAvailable();
+        });
+        consumer.start();
+        //Thread.sleep(1000);
+        producer.start();
+    }
+}
